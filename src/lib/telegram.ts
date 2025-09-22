77dbe94ad256b5f2d11d5f22e7ce27a56db2e620
Vyacheslav <slavikk015@gmail.com>
2025-09-22 11:12:00 +0300

    подправил путь для запроса на сервер для ТГ

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -1,38 +1,32 @@
 import type { OrderState } from "../store/slices/orderSlice";
 
 export const sendToTelegram = async (data: OrderState) => {
+  console.log('🚀 /api/telegram/send вызван');
   if (data.honeypot && data.honeypot.trim() !== "") {
     console.warn("Бот обнаружен, отправка отменена.");
     return;
   }
 
-  const chatId = "7911798658";
-  const message = `
-    💐 Новая заявка на букет: ${data.bouquetName}
-    👤 Имя: ${data.name}
-    📞 Телефон: ${data.phone}
-        Предпочтительный способ связи: ${data.contactMethod}
-    🌸 Предпочтения: ${data.kindOfFlowers}
-    💰 Повод: ${data.whomGifts}
-    📦 Пожелания по оформлению: ${data.flowerDesign}
-  `.trim();
-
   try {
-    const response = await fetch("/api.php", {
+    const response = await fetch("http://localhost:3002/api/telegram/send", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        chat_id: chatId,
-        text: message,
+        bouquetName: data.bouquetName,
+        name: data.name,
+        phone: data.phone,
+        contactMethod: data.contactMethod,
+        kindOfFlowers: data.kindOfFlowers,
+        whomGifts: data.whomGifts,
+        flowerDesign: data.flowerDesign,
         honeypot: data.honeypot,
-        parse_mode: "HTML",
       }),
     });
 
     if (!response.ok) {
-      throw new Error("Ошибка при отправке в Telegram");
+      throw new Error("Ошибка при отправке заявки");
     }
 
     console.log("Заявка отправлена!");
